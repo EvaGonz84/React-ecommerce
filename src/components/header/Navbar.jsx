724f36b0a01ed82de241b07000f485911d0cf946
Eva González <evagonzalez.tic@gmail.com>
2023-01-24 18:49:48 +0100

    cartdetails style added

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { FaBars, FaShoppingCart, FaTimes } from "react-icons/fa";
+import { FaBars, FaTrashAlt, FaShoppingCart, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import NavLink from "./NavLink";
+import smallImage from "../../assets/images/bread-1.jpg";
 
 const Navbar = () => {
   {
@@ -48,6 +49,35 @@ const Navbar = () => {
           <button className="pt-2">
             <FaShoppingCart />
           </button>
+          <section className="absolute top-[70px] left-0 z-10 w-full md:left-[95%] md:max-w-2xl md:-translate-x-full">
+            <div className="mx-8 rounded-md bg-white md:mx-8">
+              <h4 className="px-4 py-6 text-lg font-semibold">Cart</h4>
+              <hr />
+              <article className="grid grid-cols-[1fr_4fr_1fr] items-center gap-8 px-6 py-8 text-lg">
+                <img
+                  src={smallImage}
+                  className="rounded-md"
+                  alt="bread-image"
+                ></img>
+                <div>
+                  <p>Lorem ipsum dolor sit amet</p>
+                  <div>
+                    <span>$3x4</span>
+                    <span className="font-semibold"> $12</span>
+                  </div>
+                </div>
+                <button className="ml-auto">
+                  <FaTrashAlt className="hover:fill-yellow-600" />
+                </button>
+              </article>
+
+              <div className="px-6 pb-8">
+                <button className="w-full rounded-md bg-yellow-600 p-2 font-semibold transition-all hover:bg-yellow-500 text-lg">
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </section>
         </div>
       </header>
     </>
